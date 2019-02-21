67b4dda3d14fc627c2a8dc07303a9f0c7c67a257
Trevor Smith <smittrevor@gmail.com>
2019-02-21 10:41:44 -0600

    Configure to be served by proxy

--- client/App.js
+++ client/App.js
@@ -7,8 +7,57 @@ class App extends React.Component {
   }
 
   render() {
-    return <div>Hello?</div>;
+    return (
+      <div>
+        <div style={outerWrap}>
+          <div style={innerWrap}>
+            <div id="photos" className="gallery" style={gallery} />
+            <div className="overview" style={overview}>
+              OVERVIEW
+            </div>
+          </div>
+          <div id="similar-options" className="related" style={related} />
+        </div>
+        <div id="Reviews" />
+      </div>
+    );
   }
 }
+
+const outerWrap = {
+  width: "100%",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  height: "1150px"
+};
+
+const innerWrap = {
+  display: "flex",
+  justifyContent: "space-between"
+};
+
+const gallery = {
+  flex: ".6",
+  height: "600px"
+  // backgroundColor: "#cccccc",
+  // border: "1px solid green",
+  // color: "green"
+};
+
+const overview = {
+  flex: ".4",
+  height: "600px",
+  backgroundColor: "#cccccc",
+  border: "1px solid red",
+  color: "red"
+};
+
+const related = {
+  height: "500px"
+  // backgroundColor: "#cccccc",
+  // border: "1px solid yellow",
+  // color: "yellow"
+};
 
 ReactDOM.render(<App />, document.getElementById("app"));
--- server.js
+++ server.js
@@ -1,9 +1,48 @@
 const express = require("express");
 const path = require("path");
+const axios = require("axios");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/products/:id", (req, res) => {
+  axios
+    .get(
+      `http://ec2-3-93-54-61.compute-1.amazonaws.com/products/${req.params.id}`
+    )
+    .then(product => {
+      res.json(product.data);
+    })
+    .catch(err => {
+      console.log("Error with GET request in proxy");
+      res.sendStatus(400);
+    });
+});
+
+app.get("/getFiveRandom", (req, res) => {
+  axios
+    .get(`http://ec2-3-80-146-248.compute-1.amazonaws.com/getFiveRandom`)
+    .then(product => {
+      res.json(product.data);
+    })
+    .catch(err => {
+      console.log("Error with GET request in proxy");
+      res.sendStatus(400);
+    });
+});
+
+app.get("/products/:id/reviews", (req, res) => {
+  axios
+    .get(`http://localhost:3030/products/${req.params.id}/reviews`)
+    .then(product => {
+      res.json(product.data);
+    })
+    .catch(err => {
+      console.log("Error with GET request in proxy");
+      res.sendStatus(400);
+    });
+});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
